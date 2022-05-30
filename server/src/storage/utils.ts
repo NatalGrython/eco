8fc36bfb95fd56c29b5b3b56ded2e6c2460913aa
NatalGrypthon <altair.al-jabavi@yandex.ru>
2022-05-30 05:33:34 +0300

    Update paths

--- server/src/storage/utils.ts
+++ server/src/storage/utils.ts
@@ -3,7 +3,7 @@ function replaceAll(string: string, search: string, replace: string) {
 }
 
 export const createPath = (path: string) =>
-  `http://localhost:3001/${replaceAll(
+  `http://${process.env.HOST}:${process.env.PORT}/${replaceAll(
     path.replace('public\\', '').replace('public/', ''),
     '\\',
     '/',
